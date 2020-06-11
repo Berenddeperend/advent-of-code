f5995aea759a70ad189921fcd390abbe3635fb36
Berend Swennenhuis <berendswennenhuis@trimm.nl>
2020-06-11 10:43:32 +0200

    Some progress

--- 2018/4/4-1.js
+++ 2018/4/4-1.js
@@ -1,9 +1,10 @@
 console.time('runtime');
 
 const fs = require('fs');
-const input = fs.readFileSync('./input.txt', 'utf8').split("\n");
+const input = fs.readFileSync('./input2.txt', 'utf8').split("\n");
 
 const parsedInput = input.map(entry => {
+  
   const timestamp = entry.split("]")[0].split("[")[1];
   const date = timestamp.split(" ")[0];
   const [year, month, day] = date.split("-").map(string => Number(string));
@@ -25,7 +26,14 @@ const guards = [...new Set(parsedInput
   .map(entry => entry.log.split("#")[1].split(" ")[0])
   .map(string => Number(string)))];
 
-console.log('guards: ', guards);
+const guardLog = guards.reduce((log, guard) => {
+  log['#' +guard] = { 
+    // minutesAsleep: 
+  }
+  return log;
+}, {});
+
+console.log('guardLog: ', guardLog);
 
 
 function stupidAdd(numbers){
