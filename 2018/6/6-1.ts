92c24f4aad44d739ff13767f078475899419e6c8
Berend <berend.swennenhuis@trimm.nl>
2020-07-20 11:37:04 +0200

    Some progress

--- 2018/6/6-1.ts
+++ 2018/6/6-1.ts
@@ -1,6 +1,8 @@
 console.time("runtime");
 import { readFileStrSync } from "https://deno.land/std@0.60.0/fs/read_file_str.ts";
 
+const alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
+
 const input: Coordinates[] = readFileStrSync("./input2.txt", {
   encoding: "utf8",
 })
@@ -31,21 +33,22 @@ input.map((coordinates) => {
   return coordinates;
 });
 
-const candidates = input
-.filter(coordinates => !coordinates.isInfinite)
-.reduce((acc: Area, curr): Area => {
-  acc[curr.id] = getAreaOfCoordinates(curr);
-  return acc;
-}, {})
+
+// const candidates = input
+// .filter(coordinates => !coordinates.isInfinite)
+// .reduce((acc: Area, curr): Area => {
+//   acc[curr.id] = getAreaOfCoordinates(curr);
+//   return acc;
+// }, {})
 
 
-console.log('candidates: ', candidates);
+// console.log('candidates: ', candidates);
 
-function getAreaOfCoordinates(coordinates: Coordinates):number {
-  return 2; //todo
-}
+// function getAreaOfCoordinates(coordinates: Coordinates):number {
+//   return 2; //todo
+// }
 
-function createGrid(
+function createGrid( // also wrong
   fromX: number,
   toX: number,
   fromY: number,
@@ -53,6 +56,7 @@ function createGrid(
 ): Grid {
   let output: Grid = [];
 
+  //create skeleton
   for (let i = fromX, j = 0; i < toX + 1; i++, j++) {
     output.push([]);
     for (let k = fromY, l = 0; k < toY + 1; k++, l++) {
@@ -60,11 +64,43 @@ function createGrid(
     }
   }
 
-  input.forEach((item) => {
-    output[item.x - fromX][item.y - fromY] = "x";
+  //Add coordinates
+  input.forEach((coordinates) => {
+    output[coordinates.x - fromX][coordinates.y - fromY] = alphabet[coordinates.id].toUpperCase();
   });
 
+  //calculate nearest coordinate for every point
+  // for (let i = fromX, j = 0; i < toX + 1; i++, j++) {
+  //   for (let k = fromY, l = 0; k < toY + 1; k++, l++) {
+  //     if(output[j][l] === ".") {
+  //       output[j][l] = closestCoordinateFromPoint(i, k);
+  //     }
+  //   }
+  // }
   return output;
+}
+
+function closestCoordinateFromPoint(x:number, y:number):string {
+  let isTie = false; //tie logic doesn't work yet
+  
+  const closest = input.reduce((closestCoordinateSoFar, curr):Coordinates => {
+    let distance = getDistanceBetweenTwoPoints(curr.x, curr.y, x, y)
+    let currentClosestDistance = getDistanceBetweenTwoPoints(closestCoordinateSoFar.x, closestCoordinateSoFar.y, x, y)
+    if (distance === currentClosestDistance) {
+      isTie = true;
+    }
+    if (distance < currentClosestDistance){ 
+      closestCoordinateSoFar = curr;
+      isTie = false;
+    }
+    return closestCoordinateSoFar
+  });
+
+  return isTie ? "." : alphabet[closest.id];
+}
+
+function getDistanceBetweenTwoPoints(x1:number, y1:number, x2:number, y2:number): number {
+  return Math.abs((Math.abs(x1) - Math.abs(x2))) + Math.abs((Math.abs(y1) - Math.abs(y2)));
 }
 
 let grid = createGridStringFromGrid(createGrid(minX, maxX, minY, maxY));
@@ -73,13 +109,13 @@ function createGridStringFromGrid(grid: Grid): string {
   return grid.map((row) => row.join("")).join("\n");
 }
 
-// console.log(grid);
+console.log(grid);
 
 
 
 
 type Grid = string[][];
-type Area = {[key:string]: number}
+// type Area = {[key:string]: number}
 interface Coordinates {
   x: number;
   y: number;
