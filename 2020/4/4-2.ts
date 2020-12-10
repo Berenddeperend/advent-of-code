beca7f6cc674c065881eb76c9a179aa28f1dfa36
Berend <berend.swennenhuis@trimm.nl>
2020-12-10 23:22:14 +0100

    Finish 4.2

--- 2020/4/4-2.ts
+++ 2020/4/4-2.ts
@@ -1,7 +1,11 @@
 console.time("runtime");
 import { readFileSync } from "fs";
 
-const input = readFileSync("./4/input.txt", "utf8").split("\n\n");
+let testMode = false;
+const file = testMode ? "input-sample" : "input";
+const input = readFileSync(`./4/${file}.txt`, "utf8").split("\n\n");
+
+type Entry = { [entryKey: string]: string };
 
 const entries = input
   .map((entry) => {
@@ -15,35 +19,51 @@ const entries = input
       const [key, value] = curr.split(":");
       acc[key] = value;
       return acc;
-    }, {});
+    }, {} as Entry);
   });
 
-function entryIsValid(entry: any): boolean {
-  // const requiredProps = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"];
+function entryIsValid(entry: Entry): boolean {
   const requiredTests = {
-    byr: (d) => parseInt(d) >= 1920 && parseInt(d) <= 2002,
-    iyr: (d) => parseInt(d) >= 2010 && parseInt(d) <= 2020,
-    eyr: (d) => parseInt(d) >= 2020 && parseInt(d) <= 2030,
+    byr: (d) => d.length === 4 && parseInt(d) >= 1920 && parseInt(d) <= 2002,
+    iyr: (d) => d.length === 4 && parseInt(d) >= 2010 && parseInt(d) <= 2020,
+    eyr: (d) => d.length === 4 && parseInt(d) >= 2020 && parseInt(d) <= 2030,
     hgt: (d) => {
-      return d.includes("in")
-        ? parseInt(d) >= 59 && parseInt(d) <= 76
-        : parseInt(d) >= 150 && parseInt(d) <= 193;
+      try {
+        const { value, unit } = /(?<value>^\d+)(?<unit>in$|cm$)/gim.exec(
+          d
+        ).groups;
+        switch (unit) {
+          case "in":
+            return parseInt(value) >= 59 && parseInt(value) <= 76;
+          case "cm":
+            return parseInt(value) >= 150 && parseInt(value) <= 193;
+        }
+      } catch (e) {
+        return false;
+      }
     },
     hcl: (d) => {
-      //todo
+      return /^#[\da-f]{6}$/gim.test(d);
     },
     ecl: (d) => {
       return ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"].includes(d);
     },
     pid: (d) => {
-      //todo
+      return /^\d{9}$/gm.test(d);
     },
   };
   let availableKeys = Object.keys(entry);
   const containsAllProps = Object.keys(requiredTests).every((prop) =>
     availableKeys.includes(prop)
   );
-  return containsAllProps;
+
+  if (!containsAllProps) return false;
+
+  return Object.keys(requiredTests).every((key) => {
+    return requiredTests.hasOwnProperty(key)
+      ? requiredTests[key](entry[key])
+      : false;
+  });
 }
 
 const numberOfValidPassports = entries.filter(entryIsValid).length;
