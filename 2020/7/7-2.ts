0aa914a7a1ae457a29bb68e488085423d26ec0f5
Berend <berend.swennenhuis@trimm.nl>
2020-12-21 23:19:06 +0100

    7-2, easy peasy

---
+++ 2020/7/7-2.ts
@@ -0,0 +1,85 @@
+console.time("runtime");
+import { readFileSync } from "fs";
+
+const input = readFileSync("./7/input.txt", "utf8").split("\n");
+
+type rule = {
+  bagName: string;
+  contains: {
+    quantity: number;
+    target: string;
+  }[];
+};
+
+function parser(line: string): rule {
+  const bagName = line
+    .split(" ")
+    .filter((word, index) => index <= 1)
+    .join(" ");
+  const contains =
+    line.match(/(\d)( \w+ \w+)/gm)?.map((line) => {
+      const [quantity, ...target] = line.split(" ");
+      return {
+        quantity: parseInt(quantity),
+        target: target.join(" "),
+      };
+    }) || [];
+  return { bagName, contains };
+}
+
+const bags = input.map(parser);
+
+const memoize = (fn) => {
+  let cache = {};
+  return (...args) => {
+    let n = args[0];
+    if (n in cache) {
+      return cache[n];
+    } else {
+      let result = fn(n);
+      cache[n] = result;
+      return result;
+    }
+  };
+};
+
+function addObjs(objA, objB) {
+  const objC = { ...objA };
+  Object.entries(objB).map((tuple) => {
+    objC.hasOwnProperty(tuple[0])
+      ? (objC[tuple[0]] += tuple[1])
+      : (objC[tuple[0]] = tuple[1]);
+  });
+
+  return objC;
+}
+let iterations = 0;
+
+const downTheRabbitHole = memoize((bagName: string) => {
+  iterations++;
+  let mapping = {};
+  bags
+    .find((bag) => bag.bagName === bagName)
+    .contains.map((bag) => {
+      for (let i = 0; i < bag.quantity; i++) {
+        mapping.hasOwnProperty(bag.target)
+          ? mapping[bag.target]++
+          : (mapping[bag.target] = 1);
+
+        mapping = addObjs(mapping, downTheRabbitHole(bag.target));
+      }
+    });
+
+  return mapping;
+});
+
+const answer = Object.values(downTheRabbitHole("shiny gold")).reduce(
+  (acc: number, curr: number) => {
+    return acc + curr;
+  },
+  0
+);
+
+console.log(answer);
+
+console.timeEnd("runtime");
