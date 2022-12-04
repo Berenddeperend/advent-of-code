bdb42a736608cfc58897c031fe5a25178afd958f
Berend Swennenhuis <berendswennenhuis@gmail.com>
2022-12-04 01:02:27 +0100

    cleanup

--- 2022/3/3-2.ts
+++ 2022/3/3-2.ts
@@ -4,7 +4,7 @@ import { readFileSync } from 'fs';
 const input:string[] = readFileSync("./2022/3/input.txt", 'utf8').split('\n');
 
 const scoreChart = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-const chunkSize = 3
+const chunkSize = 3;
 
 function chunk(input, size) {
     return input.reduce((acc, curr, i) => {
@@ -20,7 +20,7 @@ function getScoreForGroup(bags: string[]) {
         curr in acc ? acc[curr]++ : acc[curr] = 1;
         return acc;
     }, {});
-    const badgeItem = Object.entries(itemMappings).find(entry => entry[1] === 3)[0];
+    const badgeItem = Object.entries(itemMappings).find(entry => entry[1] === chunkSize)[0];
 
     return scoreChart.indexOf(badgeItem) + 1;
 }
